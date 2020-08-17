68876c7612295c13200dba6ab205a6616f84d5f8
miguelfreitas18 <miguelfreitas8000@gmail.com>
2020-08-17 10:31:38 -0300

    Date Picker dialogs e melhoria da CourseDetailsActivity

--- TesteCastGroupApp/app/src/main/java/com/miguelbrmfreitas/testecastgroup/activities/CourseDetailsActivity.java
+++ TesteCastGroupApp/app/src/main/java/com/miguelbrmfreitas/testecastgroup/activities/CourseDetailsActivity.java
@@ -13,7 +13,10 @@ import android.widget.NumberPicker;
 import com.miguelbrmfreitas.testecastgroup.R;
 import com.miguelbrmfreitas.testecastgroup.components.CustomButton;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -33,6 +36,15 @@ public class CourseDetailsActivity extends AppCompatActivity implements DatePick
     private NumberPicker mCategoryPicker;
 
     private CustomButton mCustomButton;
+
+    private DatePickerDialog.OnDateSetListener mDate = this;
+
+    Calendar mCalendar = Calendar.getInstance();
+
+    private final int START_DATE = 1;
+    private final int END_DATE = 2;
+
+    private int mDatePickerClicked = START_DATE;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -64,6 +76,7 @@ public class CourseDetailsActivity extends AppCompatActivity implements DatePick
 
         // Só faz se estiver editando (PUT)
         if (isEditing) {
+            // Recupera do bundle
             Date startDate = new Date(bundle.getLong("start_date"));
             Date endDate = new Date(bundle.getLong("end_date"));
             String description = bundle.getString("description");
@@ -78,10 +91,63 @@ public class CourseDetailsActivity extends AppCompatActivity implements DatePick
             //mStudentsPicker.setValue(studentsNumber);
             mCategoryPicker.setValue(categoryCode);
         }
+
+        // Abre o dialog para escolher uma data inicial
+        mStartDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createDateDialog(true);
+            }
+        });
+
+        // Abre o dialog para escolher uma data final
+        mEndDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createDateDialog(false);
+            }
+        });
     }
 
     @Override
     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        String myFormat = "dd/MM/yyyy"; // Formato da data
+        final SimpleDateFormat sdf = new SimpleDateFormat(myFormat, new Locale("pt","BR")); // Colocando a formatação em PT-BR
 
+        // Atualiza o objeto de calendário
+        mCalendar.set(Calendar.YEAR, datePicker.getYear());
+        mCalendar.set(Calendar.MONTH, datePicker.getMonth());
+        mCalendar.set(Calendar.DAY_OF_MONTH, datePicker.getDayOfMonth());
+
+        // Altera o edit text correto
+        if (mDatePickerClicked == START_DATE) {
+            updateStartDateLabel(sdf);
+        } else {
+            updateEndDateLabel(sdf);
+        }
+    }
+
+    /**
+     * Coloca no edit text de data inicial a data escolhida
+     * @param sdf   Formatação da data
+     */
+    private void updateStartDateLabel(SimpleDateFormat sdf) {
+        mStartDatePicker.setText(sdf.format(mCalendar.getTime()));
+    }
+
+    /**
+     * Coloca no edit text de data final a data escolhida
+     * @param sdf   Formatação da data
+     */
+    private void updateEndDateLabel(SimpleDateFormat sdf) {
+        mEndDatePicker.setText(sdf.format(mCalendar.getTime()));
+    }
+
+    private void createDateDialog(boolean isStart) {
+        mDatePickerClicked = isStart? START_DATE: END_DATE; // Seta o date clicado
+        // Cria o dialog para receber o input da data e chama o método para visualizar
+        new DatePickerDialog(CourseDetailsActivity.this, mDate, mCalendar
+                .get(Calendar.YEAR), mCalendar.get(Calendar.MONTH),
+                mCalendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
